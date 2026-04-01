e97f217d865bfa579748b29d059edb63916907d2
Ayush Raj Tiwari <harshittiwari308@gmail.com>
2026-04-01 14:15:22 +0530

    Fixed deployment issue - added port binding

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def index():
     return render_template('index.html', table=table, summary=summary, columns=columns,graph=graph, graph_data=None,labels=None,graph_type=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
